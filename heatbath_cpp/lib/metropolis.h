3dcb370ac83038a94026d817f79359f48aaba0a4
Gurtej Kanwar <gurtej@mit.edu>
2025-07-10 17:45:58 +0100

    hb: Fix rng clobbering

--- heatbath_cpp/lib/metropolis.h
+++ heatbath_cpp/lib/metropolis.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <random>
 #include "util.h"
 
@@ -9,32 +10,31 @@ template<typename Action, typename Config, typename Proposal>
 double metropolis_update(
     const Action& action, Config& cfg, const Proposal& propose, my_rand_arr& rng) {
   std::uniform_real_distribution<double> unif_dist;
-  // split volume
-  // const unsigned block_size = cfg.geom.vol >= N_BLOCK ? cfg.geom.vol/N_BLOCK : 1;
   // split axis 0
-  const unsigned block_size = cfg.geom.dims[0] >= N_BLOCK ? cfg.geom.dims[0]/N_BLOCK : 1;
+  // const unsigned block_size = cfg.geom.dims[0] >= N_BLOCK ? cfg.geom.dims[0]/N_BLOCK : 1;
   int acc = 0;
 
-  // even stripes
-#pragma omp parallel for schedule(static,)
-  for (ull x0 = 0; x0 < cfg.geom.dims[0]; ++x0) {
-    if (x0 % 2 != 0) continue;
-    auto& thread_rng = rng[x0 / block_size];
-    for (ull x1 = 0; x1 < cfg.geom.dims[1]; ++x1) {
-      ull x = cfg.geom.get_idx({x0, x1});
-      // standard MH update
-      const double old_S = action.local_action(cfg, x);
-      const typename Config::Spin_t z = cfg.z[x];
-      cfg.z[x] = propose(z, thread_rng);
-      const double new_S = action.local_action(cfg, x);
-      if (unif_dist(thread_rng) < exp(-new_S + old_S)) {
-        acc++;
-      }
-      else {
-        cfg.z[x] = z;
-      }
-    }
-  }
+  // // even stripes
+  // #pragma omp parallel for schedule(static,)
+  // for (ull x0 = 0; x0 < cfg.geom.dims[0]; ++x0) {
+  //   if (x0 % 2 != 0) continue;
+  //   auto& thread_rng = rng[x0 / block_size];
+  //   for (ull x1 = 0; x1 < cfg.geom.dims[1]; ++x1) {
+  //     ull x = cfg.geom.get_idx({x0, x1});
+  //     assert(x < cfg.geom.vol);
+  //     // standard MH update
+  //     const double old_S = action.local_action(cfg, x);
+  //     const typename Config::Spin_t z = cfg.z[x];
+  //     cfg.z[x] = propose(z, thread_rng);
+  //     const double new_S = action.local_action(cfg, x);
+  //     if (unif_dist(thread_rng) < exp(-new_S + old_S)) {
+  //       acc++;
+  //     }
+  //     else {
+  //       cfg.z[x] = z;
+  //     }
+  //   }
+  // }
 
   // // odd stripes
   // #pragma omp parallel for
@@ -57,44 +57,32 @@ double metropolis_update(
   //   }
   // }
 
-  // even sites
-  #pragma omp parallel for
-  for (ull x = 0; x < cfg.geom.vol; ++x) {
-    // check parity even
-    assert(cfg.geom.dims.size() == 2);
-    auto coord_x = cfg.geom.coord(x);
-    const int parity = (coord_x[0] + coord_x[1]) % 2;
-    if (parity != 0) continue;
-    // standard MH update
-    const double old_S = action.local_action(cfg, x);
-    const typename Config::Spin_t z = cfg.z[x];
-    cfg.z[x] = propose(z, rng[x / block_size]);
-    const double new_S = action.local_action(cfg, x);
-    if (unif_dist(rng[x / block_size]) < exp(-new_S + old_S)) {
-      acc++;
-    }
-    else {
-      cfg.z[x] = z;
-    }
-  }
-  // odd sites
-  #pragma omp parallel for
-  for (ull x = 0; x < cfg.geom.vol; ++x) {
-    // check parity odd
-    assert(cfg.geom.dims.size() == 2);
-    auto coord_x = cfg.geom.coord(x);
-    const int parity = (coord_x[0] + coord_x[1]) % 2;
-    if (parity != 1) continue;
-    // standard MH update
-    const double old_S = action.local_action(cfg, x);
-    const typename Config::Spin_t z = cfg.z[x];
-    cfg.z[x] = propose(z, rng[x / block_size]);
-    const double new_S = action.local_action(cfg, x);
-    if (unif_dist(rng[x / block_size]) < exp(-new_S + old_S)) {
-      acc++;
-    }
-    else {
-      cfg.z[x] = z;
+  // split volume
+  const unsigned block_size = cfg.geom.vol >= N_BLOCK ? cfg.geom.vol/N_BLOCK : 1;
+
+  // checkboard update
+  for (ull p = 0; p < 2; ++p) {
+    #pragma omp parallel for
+    for (ull b = 0; b < N_BLOCK; ++b) {
+      auto& thread_rng = rng[b];
+      for (ull x = b*block_size; x < (b+1)*block_size && x < cfg.geom.vol; ++x) {
+        // check parity
+        assert(cfg.geom.dims.size() == 2);
+        auto coord_x = cfg.geom.coord(x);
+        const int parity = (coord_x[0] + coord_x[1]) % 2;
+        if (parity != p) continue;
+        // standard MH update
+        const double old_S = action.local_action(cfg, x);
+        const typename Config::Spin_t z = cfg.z[x];
+        cfg.z[x] = propose(z, thread_rng);
+        const double new_S = action.local_action(cfg, x);
+        if (unif_dist(thread_rng) < exp(-new_S + old_S)) {
+          acc++;
+        }
+        else {
+          cfg.z[x] = z;
+        }
+      }
     }
   }
 
