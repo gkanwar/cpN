596d349e8a93c47cca8abbcc788fb9f573283d69
Gurtej Kanwar <gurtej@mit.edu>
2026-04-29 17:03:00 +0100

    hb: Update ens params

--- heatbath_py/src/test_heatbath.py
+++ heatbath_py/src/test_heatbath.py
@@ -1,43 +1,49 @@
 from lib.cpn import *
 from lib.cpn_heatbath import *
 
+import analysis as al
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def main():
-    beta = 3.0
+    beta = 6.0
     action = CPNQuarticAction(beta)
-    N = 2
-    shape = (32,32)
+    N = 3
+    shape = (4,4)
     seed = 43298
     rng = np.random.default_rng(seed)
-    # z0 = make_cold_cfg(shape, N)
+    z0 = make_cold_cfg(shape, N)
     # z0 = make_hot_cfg(shape, N, rng=rng)
-    z0 = make_topo_cfg(shape, N)
+    # z0 = make_topo_cfg(shape, N)
     sigma = 0.5
     params = dict(
         action=action,
         sigma=sigma,
-        n_iter=1000,
-        n_therm=0,
+        n_iter=100000,
+        n_therm=100,
         n_skip=10,
         rng=rng
     )
     res = run_metropolis(z0, **params)
-    wflow_params = dict(dt=0.05, n_step=500)
-    Q = [np.sum(topological_charge(wflow(z, **wflow_params))) for z in tqdm.tqdm(res['ens'])]
-    Q = np.array(Q)
-    X2 = [measure_2pt_susc(z) for z in tqdm.tqdm(res['ens'])]
+    # wflow_params = dict(dt=0.05, n_step=500)
+    # Q = [np.sum(topological_charge(wflow(z, **wflow_params))) for z in tqdm.tqdm(res['ens'])]
+    # Q = np.array(Q)
+    # X2 = [measure_2pt_susc(z) for z in tqdm.tqdm(res['ens'])]
 
-    fig, axes = plt.subplots(3,1, tight_layout=True, figsize=(6,8))
-    axes[0].plot(res['S'])
-    axes[0].set_ylabel('S')
-    axes[1].plot(Q, marker='.')
-    axes[1].set_ylabel('Q')
-    axes[2].plot(X2)
-    axes[2].set_ylabel('susc')
+    fig, ax = plt.subplots(1,1, tight_layout=True, figsize=(6,4))
+    ax.plot(res['S'])
+    ax.set_ylabel('S')
+    # axes[1].plot(Q, marker='.')
+    # axes[1].set_ylabel('Q')
+    # axes[2].plot(X2)
+    # axes[2].set_ylabel('susc')
     fig.savefig('test_heatbath.pdf')
+
+    est_S = al.bootstrap(res['S'], Nboot=100, f=al.rmean)
+    print(f'{est_S=}')
+
+    np.save(f'cpn_b{beta:.1f}.npy', res['ens'])
     
 if __name__ == '__main__':
     main()
